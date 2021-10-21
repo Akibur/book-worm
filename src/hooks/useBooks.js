e764283b5bce93a190167c2e7d1962a456c84f1e
Akib <akib3366@gmail.com>
2021-10-21 20:49:56 +0600

    useBook custom hook to get book data

---
+++ src/hooks/useBooks.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const useBooks = () => {
+    const [books, setBooks] = useState([]);
+    const [displayBooks, setDisplayBooks] = useState([]);
+
+    useEffect(() => {
+        fetch('./data.json')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setBooks(data);
+                setDisplayBooks(data);
+            }).catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    return [
+        books,
+        displayBooks,
+        setDisplayBooks
+    ];
+};
+
+export default useBooks;
