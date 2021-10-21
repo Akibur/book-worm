2ffd5eea62f7002d40f93f90aa2142701b048cd5
Akib <akib3366@gmail.com>
2021-10-21 23:45:53 +0600

    About and contact page added

---
+++ src/pages/About/About.js
@@ -0,0 +1,21 @@
+import { Container } from '@mui/material';
+import React from 'react';
+
+export default function About() {
+    return (
+        <Container sx={{ msx: 'auto', textAlign: 'center', my: 20 }}>
+            <h1>About Us</h1>
+            <p>Book Worm  is an online bookstore with a mission to financially support local, independent bookstores.
+                We believe that bookstores are essential to a healthy culture. They’re where authors can connect with readers, where we discover new writers, where children get hooked on the thrill of reading that can last a lifetime. They’re also anchors for our downtowns and communities.
+                As more and more people buy their books online, we wanted to create an easy, convenient way for you to get your books and support bookstores at the same time.
+            </p>
+            <p>
+                By design, we give away over 75% of our profit margin to stores, publications, authors and others who make up the thriving, inspirational culture around books!
+            </p>
+            <p>
+                We hope that Book Worm can help strengthen the fragile ecosystem and margins around bookselling and keep local bookstores an integral part of our culture and communities.
+
+            </p>
+        </Container >
+    );
+}
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,29 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Button, Container, TextField } from '@mui/material';
+import classes from './Contact.module.css';
+
 
 export default function Contact() {
     return (
-        <Fragment>
-            <h1>Contact</h1>
-        </Fragment>
+        <Container sx={{ mx: 'auto', textAlign: 'center', my: 25 }}>
+            <div className={classes.loginForm}  >
+                <h2>Contact Us</h2>
+                <form>
+                    <TextField sx={{ width: 1, my: 1 }} id="outlined-basic" label="Name" variant="outlined" />
+                    <br />
+                    <TextField sx={{ width: 1, my: 1 }} id="outlined-basic" label="Email" variant="outlined" />
+                    <br />
+                    <TextField
+                        sx={{ width: 1, my: 1 }}
+                        id="outlined-multiline-static"
+                        label="Message"
+                        multiline
+                        rows={4}
+                    />
+                    <br />
+                    <Button sx={{ width: 1, my: 1 }} variant="contained">Send</Button>
+                </form>
+            </div>
+        </Container >
     );
 }
